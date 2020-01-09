325ca24065e01d628c9e68dee66f59267fd54e70
Jonathan-Todd-MF <Jonathan.Todd@microfocus.com>
2020-01-09 11:38:39 +0000

    practice

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { createHttpObservable } from '../common/util';
+
 
 @Component({
   selector: 'about',
@@ -10,6 +12,52 @@ export class AboutComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+
+    const http$ = createHttpObservable('/api/courses');
+
+    const sub = http$.subscribe(console.log);
+
+    setTimeout(() => sub.unsubscribe(), 0);
+
+
+    // const interval1$ = interval(1000);
+
+    // const interval2$ = interval1$.pipe(map(val => 10 * val));
+
+    // const result$ = merge(interval1$, interval2$);
+
+    // result$.subscribe(console.log)
+
+    // const source1$ = of(1, 2, 3);
+
+    // const source2$ = of(4, 5, 6);
+
+    // const source3$ = of(7, 8, 9);
+
+    // const result$ = concat(source1$, source2$, source3$);
+
+    // result$.subscribe(
+    //   console.log
+    // );
+
+
+    // const interval$ = timer(3000, 1000);
+
+    // const sub = interval$.subscribe(val => console.log('stream 1 => ' + val));
+
+    // setTimeout(() => sub.unsubscribe(), 5000);
+
+    // const click$ = fromEvent(document, 'click');
+
+    // click$.subscribe(
+    //   val => console.log('click stream =>' + val),
+
+    //   err => console.log(err),
+
+    //   () => console.log('completed')
+    // );
+
   }
 
 }
+
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,7 +1,8 @@
-import {Component, OnInit} from '@angular/core';
-import {Course} from "../model/course";
-import {interval, Observable, of, timer} from 'rxjs';
-import {catchError, delayWhen, map, retryWhen, shareReplay, tap} from 'rxjs/operators';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { map, shareReplay, tap } from 'rxjs/operators';
+import { createHttpObservable } from '../common/util';
+import { Course } from '../model/course';
 
 
 @Component({
@@ -11,6 +12,9 @@ import {catchError, delayWhen, map, retryWhen, shareReplay, tap} from 'rxjs/oper
 })
 export class HomeComponent implements OnInit {
 
+    beginnerCourses$: Observable<Course[]>;
+
+    advancedCourses$: Observable<Course[]>;
 
     constructor() {
 
@@ -18,7 +22,41 @@ export class HomeComponent implements OnInit {
 
     ngOnInit() {
 
+        const http$ = createHttpObservable('/api/courses');
 
+        const courses$: Observable<Course[]> = http$
+            .pipe(
+                tap(() => console.log('HTTP request exectuted')),
+                map(res => Object.values(res["payload"])),
+                shareReplay()
+            );
+
+        courses$.subscribe();
+
+        this.beginnerCourses$ = courses$
+            .pipe(
+                map(courses => courses
+                    .filter(course => course.category === 'BEGINNER'))
+            );
+
+        this.advancedCourses$ = courses$
+            .pipe(
+                map(courses => courses
+                    .filter(course => course.category === 'ADVANCED')
+                )
+            );
+
+        // courses$.subscribe(
+        //     courses => {
+
+        //         this.beginnerCourses = ;
+        //         this.advancedCourses = courses
+        //             .filter(course => course.category === "ADVANCED");
+
+        //     },
+        //     noop,
+        //     () => console.log('completed')
+        // );
 
     }
 
